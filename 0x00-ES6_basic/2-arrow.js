94ea106f9f667e00dd34b00971b47b36c08a118e
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-09-30 18:05:10 +0000

    you

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+export default function getNeighborhoodsList() {
+  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const addNeighborhood = newNeighborhood => {
+    sanFranciscoNeighborhoods.push(newNeighborhood);
+    return sanFranciscoNeighborhoods;
+  };
+
+  return { addNeighborhood };
+}
---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,3 @@
+export default function returnHowManyArguments(...noOfArgs) {
+  return noOfArgs.length;
+}
