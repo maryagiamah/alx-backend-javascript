b61f775ab2cb1fbf241b6aec988840ab63dd4f02
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-02 01:11:47 +0000

    you'

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,10 @@
-export default function createIteratorObject(report) {
-
+export default function* createIteratorObject(report) {
+  let iterCount = 0;
+  for (const vals of Object.values(report.allEmployees)) {
+    for (const name of vals) {
+      iterCount += 1;
+      yield name;
+    }
+  }
+  return iterCount;
 }
