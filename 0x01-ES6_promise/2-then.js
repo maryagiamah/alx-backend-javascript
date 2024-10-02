e1b243ad8a0da52526a8b95e54920ffd028ce216
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-02 20:57:57 +0000

    you'

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
   return promise.finally(() => {
     console.log('Got a response from the API');
   }).then(
-    (result) => ({ status: 200, body: result }),
+    (result) => ({ status: 200, body: 'success' }),
   ).catch(() => new Error());
 }
