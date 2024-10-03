6d1bf8d7a2dbadbcc34d75af48e788ecaec8f727
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-03 06:56:57 +0000

    you'

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,13 +5,11 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((values) => {
-      const newArr = [];
-      values.forEach((promise) => {
-        if (promise.status === 'fufilled') {
-          newArr.push({ status: promise.status, value: promise.value });
-        } else {
-          newArr.push({ status: promise.status, value: `${promise.reason}` });
+      const newArr = values.map((promise) => {
+        if (promise.status === 'fulfilled') {
+          return ({ status: promise.status, value: promise.value });
         }
+        return ({ status: promise.status, value: `${promise.reason}` });
       });
       return newArr;
     });
