0315c26309528a68c4a0b1f4300581befddc1112
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-12 02:36:02 +0000

    you

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+  try {
+    const newBuffer = new ArrayBuffer(length);
+    const view  = new DataView(newBuffer);
+    view.setInt8(position, value);
+    return view;
+  } catch (err) {
+    throw new Error('Position outside range');
+  }
+}
