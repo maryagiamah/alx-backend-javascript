75006cebef83174ba1d1ec6afa45dc401e35dee5
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-12 16:32:47 +0000

    you

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,8 @@
+export default function updateUniqueItems(newMap) {
+  if (!newMap || newMap instanceof Map === false) {
+    throw new Error('Cannot proces');
+  }
+  for (const [key, val] of newMap) {
+    newMap.set(key, val === 1 ? 100 : val);
+  }
+}
