d72949e14c1822716107fd8c7a6dad393d09416f
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-26 16:57:25 +0100

    you

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,31 +2,32 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, val) => {
+    fs.readFile(path, (err, val) => {
       if (err) {
-        reject(Error('Cannot load the database'));
-      }
-      let data = val.split('\n').filter((row) => row.trim() !== '');
-      data = data.slice(1);
+        reject(new Error('Cannot load the database'));
+      } else {
+        let data = val.toString().split('\n').filter((row) => row.trim() !== '');
+        data = data.slice(1);
 
-      const fields = {};
-      console.log(`Number of students: ${data.length}`);
-      data.forEach((row) => {
-        const field = row.split(',').slice(-1);
+        const fields = {};
+        console.log(`Number of students: ${data.length}`);
+        data.forEach((row) => {
+          const field = row.split(',').slice(-1);
 
-        if (!fields[field]) {
-          fields[field] = [];
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+
+          fields[field].push(row.split(',').slice(0, 1));
+        });
+        for (const field of Object.keys(fields)) {
+          const numStud = fields[field].length;
+          const studList = fields[field].join(', ');
+
+          console.log(`Number of students in ${field}: ${numStud}. List: ${studList}`);
         }
-
-        fields[field].push(row.split(',').slice(0, 1));
-      });
-      for (const field of Object.keys(fields)) {
-        const numStud = fields[field].length;
-        const studList = fields[field].join(', ');
-
-        console.log(`Number of students in ${field}: ${numStud}. List: ${studList}`);
+        resolve(val);
       }
-      resolve(val);
     });
   });
 }
