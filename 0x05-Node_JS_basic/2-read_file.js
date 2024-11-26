fca38c272b7bb164d032bf17f6cb6cacafb0d4d8
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-26 15:41:09 +0100

    you

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,12 +1,32 @@
-fs = require('fs');
+const fs = require('fs');
 
 function countStudents(path) {
-  data = fs.readFileSync(path, 'utf-8');
-  
-  console.log(data);
-  data = data.trim().split(',');
+  try {
+    let data = fs.readFileSync(path, 'utf-8');
 
-  console.log(data[1][1]);
+    data = data.split('\n').filter((row) => row.trim() !== '');
+    data = data.slice(1);
+
+    const fields = {};
+    console.log(`Number of students: ${data.length}`);
+    data.forEach((row) => {
+      const field = row.split(',').slice(-1);
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(row.split(',').slice(0, 1));
+    });
+    for (const field of Object.keys(fields)) {
+      const numStud = fields[field].length;
+      const studList = fields[field].join(', ');
+
+      console.log(`Number of students in ${field}: ${numStud}. List: ${studList}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
