c0cb2c9f2e3df89a1da924ecd88231818867e419
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-26 16:25:53 +0100

    you

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, val) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+      }
+      let data = val.split('\n').filter((row) => row.trim() !== '');
+      data = data.slice(1);
+
+      const fields = {};
+      console.log(`Number of students: ${data.length}`);
+      data.forEach((row) => {
+        const field = row.split(',').slice(-1);
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(row.split(',').slice(0, 1));
+      });
+      for (const field of Object.keys(fields)) {
+        const numStud = fields[field].length;
+        const studList = fields[field].join(', ');
+
+        console.log(`Number of students in ${field}: ${numStud}. List: ${studList}`);
+      }
+      resolve(val);
+    });
+  });
+}
+
+module.exports = countStudents;
