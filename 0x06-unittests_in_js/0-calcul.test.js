b79c8601abe4e3db4c93cc8c3e7d16a9e03a8d62
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-28 14:53:45 +0100

    you

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,6 +3,6 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('adds a and b', () => {
-    assert.equal(5, calculateNumber(3, 2));
+    assert.equal(6, calculateNumber(3.8, 2.3));
   });
 });
