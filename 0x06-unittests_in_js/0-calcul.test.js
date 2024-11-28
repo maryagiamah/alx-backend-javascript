191848492a7e66269fa883100ca298543a834a31
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-28 14:59:33 +0100

    you

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,18 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('adds a and b', () => {
+  it('rounds both a and b', () => {
     assert.equal(6, calculateNumber(3.8, 2.3));
+    assert.equal(13, calculateNumber(6.3, 7.1));
+  });
+
+  it('rounds only a', () => {
+    assert.equal(6, calculateNumber(3.8, 2));
+    assert.equal(13, calculateNumber(6.3, 7));
+  });
+
+  it('rounds only b', () => {
+    assert.equal(5, calculateNumber(3, 2.3));
+    assert.equal(13, calculateNumber(6, 7.1));
   });
 });
