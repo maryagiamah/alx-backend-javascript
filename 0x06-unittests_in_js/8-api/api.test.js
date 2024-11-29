565135fe9351c1935844a211f0ee3c053ea28e99
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-29 17:09:27 +0100

    you

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const app = require('./api')
+const axios = require('axios');
+
+describe('testApp', () => {
+  it('testAppIndexPage', (done) => {
+    axios.get('http://localhost:7865').then((res) => {
+      assert.equal(res.status, 200);
+
+      assert.equal(res.data, 'Welcome to the payment system');
+
+      assert.equal(res.headers['content-length'], 29);
+      done();
+    });
+  });
+});
