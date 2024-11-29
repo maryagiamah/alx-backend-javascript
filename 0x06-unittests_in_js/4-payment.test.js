c3c68f15422126b40dcd226b59d660e1c3ad16d6
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-29 13:37:11 +0100

    you

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,26 @@
+const sinon = require('sinon');
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require('./4-payment.js');
+
+describe('sendPaymentRequestToApi', () => {
+  it('calls calculateNumber correctly', () => {
+    const stub_func = sinon.stub(Utils, "calculateNumber");
+    const spy_func = sinon.spy(console, 'log');
+    stub_func.returns(10);
+
+    sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.called(stub_func);
+    sinon.assert.alwaysCalledWith(stub_func,'SUM', 100, 20);
+
+    sinon.assert.alwaysCalledWith(spy_func, 'The total is: 10');
+    sinon.assert.called(spy_func);
+
+    spy_func.restore();
+    stub_func.resetBehavior();
+  });
+});
