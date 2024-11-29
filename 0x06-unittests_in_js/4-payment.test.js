67397125d06b44e68b8c44ecdb3e2764d3ada97d
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-29 14:02:18 +0100

    you

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -8,12 +8,15 @@ describe('sendPaymentRequestToApi', () => {
     const spy_func = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
 
-    sinon.assert.calledOnce(stub_func);
-    sinon.assert.calledWith(stub_func, 'SUM', 100, 20);
+    sinon.assert.called(stub_func);
+    sinon.assert.alwaysCalledWith(stub_func, 'SUM', 100, 20);
 
-    sinon.assert.calledOnce(spy_func);
-    sinon.assert.calledWith(spy_func, 'The total is: 10');
+    sinon.assert.called(spy_func);
+    sinon.assert.alwaysCalledWith(spy_func, 'The total is: 10');
 
     spy_func.restore();
     stub_func.restore();
