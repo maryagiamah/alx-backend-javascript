782e716d6ce600a99ae6599f124efd454bc5137c
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-30 01:04:05 +0100

    you

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,24 +1,22 @@
 const sinon = require('sinon');
-const Utils = require("./utils");
+const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment.js');
 
 describe('testSendPaymentRequestToApi', () => {
-  it('checkUtils.calculateNumber', () => {
-    const stub_func = sinon.stub(Utils, "calculateNumber").returns(10);
+  it('should stub Utils.calculateNumber and spy on console.log', () => {
+    const stub_func = sinon.stub(Utils, 'calculateNumber').returns(10);
+
     const spy_func = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
-    sendPaymentRequestToApi(100, 20);
-    sendPaymentRequestToApi(100, 20);
-    sendPaymentRequestToApi(100, 20);
 
-    sinon.assert.called(stub_func);
-    sinon.assert.alwaysCalledWith(stub_func, 'SUM', 100, 20);
+    sinon.assert.calledOnce(stub_func);
+    sinon.assert.calledWithExactly(stub_func, 'SUM', 100, 20);
 
-    sinon.assert.called(spy_func);
-    sinon.assert.alwaysCalledWith(spy_func, 'The total is: 10');
+    sinon.assert.calledOnce(spy_func);
+    sinon.assert.calledWithExactly(spy_func, 'The total is: 10');
 
-    spy_func.restore();
     stub_func.restore();
+    spy_func.restore();
   });
 });
